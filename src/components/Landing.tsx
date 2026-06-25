a4176168d1c80d502e9577912caa6a608e1a5f4b
TECSAA WEBSITE'S <sales.tecsaa@gmail.com>
2026-06-25 18:13:18 +0530

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -15,7 +15,7 @@ const Landing = ({ children }: PropsWithChildren) => {
             </h1>
           </div>
           <div className="landing-info">
-            <h3>Top Rated Plus</h3>
+            <h3>Specialist</h3>
             <h2 className="landing-info-h2">
               <div className="landing-h2-1">Video</div>
               <div className="landing-h2-2">Graphic</div>
